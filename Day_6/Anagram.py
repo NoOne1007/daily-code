9adf457eccc5ecf9906683e32248a8616240ff0e
NoOne <harshgtm01@gmail.com>
2026-03-31 08:40:56 +0530

    (refactor): Added more optimal solution using dictonaries (python Hash Tables) both manual and using Collections module

--- Day_6/Anagram.py
+++ Day_6/Anagram.py
@@ -5,15 +5,43 @@ sys.stdin = open('input.txt', 'r')
 # Enter your code here. Read input from STDIN. Print output to STDOUT
 class Solution:
     def areAnagrams(self, s1, s2):
-       if len(s1) == len(s2):
-           for i in s1:
-               if i in s2:
-                    s2 = s2.replace(i, '', 1)
-               else:
-                    return False
-       else:
+        dict_1 = {}
+        dict_2 = {}
+        if len(s1) == len(s2):
+            for i in range(len(s1)):
+                if s1[i] not in dict_1:
+                    dict_1[s1[i]] =  1
+                else:
+                    dict_1[s1[i]] += 1
+            
+                if s2[i] not in dict_2:
+                    dict_2[s2[i]] =  1
+                else:
+                    dict_2[s2[i]] += 1
+            return (dict_1 == dict_2)
+        else:
             return False
-       return True
+
+
+
+
+# import collections
+# class Solution:
+#     def areAnagrams(self, s1, s2):
+#         return collections.Counter(s1) == collections.Counter(s2)
+
+
+# class Solution:
+#     def areAnagrams(self, s1, s2):
+#        if len(s1) == len(s2):
+#            for i in s1:
+#                if i in s2:
+#                     s2 = s2.replace(i, '', 1)
+#                else:
+#                     return False
+#        else:
+#             return False
+#        return True
 
 
 if __name__ == "__main__":
