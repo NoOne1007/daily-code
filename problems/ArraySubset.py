d9ae68a395614e09962ca070d0d648a59e4cc41c
NoOne <harshgtm01@gmail.com>
2026-04-07 23:35:01 +0530

    feat(optimize): Added optimized solution of Array Subset Problem from Geeks for Geeks

--- problems/ArraySubset.py
+++ problems/ArraySubset.py
@@ -1,9 +1,28 @@
+#User function Template for python3
+import collections
 class Solution:
     #Function to check if a is a subset of b.
     def isSubset(self, a, b):
-        for i in b:
-            if i in a:
-                a.remove(i)
+        d1 = collections.Counter(a)
+        d2 = collections.Counter(b)
+        for x in d2.keys():
+            if x in d1:
+                if d2[x] > d1[x]:
+                    return False
             else:
                 return False
         return True
+
+
+
+### Solution below is not optimized
+
+# class Solution:
+#     #Function to check if a is a subset of b.
+#     def isSubset(self, a, b):
+#         for i in b:
+#             if i in a:
+#                 a.remove(i)
+#             else:
+#                 return False
+#         return True
