78dfb560383ff9786579f9bf10a689b1b8ac460c
NoOne <harshgtm01@gmail.com>
2026-04-17 00:16:01 +0530

    feat(solution): Added  solution using for Rotating an Array problem using List slicing(the website isn't accepting the solution, but I've checked manually it works as intended) from Geeks for Geeks

--- problems/RotatinganArray.py
+++ problems/RotatinganArray.py
@@ -1,8 +1,10 @@
 class Solution:
     def leftRotate(self, arr, d):
-        L = []
-        for i in range(d, len(arr)):
-            L.append(arr[i])
-        for j in range(d):
-            L.append(arr[j])
-        return L
+        # L = []
+        # for i in range(d, len(arr)):
+        #     L.append(arr[i])
+        # for j in range(d):
+        #     L.append(arr[j])
+        # return L
+        
+        return arr[d:] + arr[:d]
