0dc35c3293a9dedc8fce1f98f7fcb018eb90fe2b
NoOne <harshgtm01@gmail.com>
2026-04-17 00:24:51 +0530

    feat(solution): Added solution for Count Odd and Even problem from Geeks for Geeks

---
+++ problems/CountOddandEven.py
@@ -0,0 +1,10 @@
+class Solution:
+	def countOddEven(self, arr):
+		odd_count = 0
+		even_count = 0
+		for i in arr:
+		    if i%2 == 0:
+		        even_count += 1
+		    else:
+		        odd_count += 1
+		return [odd_count, even_count]
