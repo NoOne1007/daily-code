712d47cadcddd880ee560024933275fd2bf69817
NoOne <harshgtm01@gmail.com>
2026-04-16 23:56:26 +0530

    feat(solution): Added solution for Array Search (2nd problem) from Geeks for Geeks

--- problems/ArraySearch.py
+++ problems/ArraySearch.py
@@ -1,6 +1,17 @@
+# class Solution:
+#     def search(self, arr, x):
+#         for i in range(len(arr)):
+#             if arr[i] == x:
+#                 return i
+#         return -1
+    
+
+
+from typing import List
+
 class Solution:
-    def search(self, arr, x):
+    def search(self, k: int, arr: List[int]) -> int:
         for i in range(len(arr)):
-            if arr[i] == x:
-                return i
+            if arr[i] == k:
+                return i+1
         return -1
