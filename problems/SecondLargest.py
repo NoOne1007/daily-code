67201577a263e022c5dcf5705651a0cd6355b7d1
NoOne <harshgtm01@gmail.com>
2026-04-20 00:13:26 +0530

    feat(solution): Added more optimized solution for Second Largest problem from Geeks for Geeks

--- problems/SecondLargest.py
+++ problems/SecondLargest.py
@@ -1,7 +1,20 @@
 class Solution:
     def getSecondLargest(self, arr):
-        try:
-            unique = sorted(set(arr), reverse=True)
-            return unique[1]
-        except:
+        largest = float('-inf')
+        second_largest = float('-inf')
+        for i in arr:
+            if i > largest:
+                second_largest, largest = largest, i
+            elif largest > i > second_largest:
+                second_largest = i
+        if second_largest > float('-inf'):
+            return second_largest
+        else:
             return -1
+            
+            
+        # try:
+        #     unique = sorted(set(arr), reverse=True)
+        #     return unique[1]
+        # except IndexError:
+        #     return -1
