5e522e39a2ada5dd5ee40329d07ebfbce03875db
NoOne <harshgtm01@gmail.com>
2026-04-19 23:47:38 +0530

    feat(solution): Added optimized solution for the Missing in Array problem from Geeks for Geeks

--- problems/MissingInArray.py
+++ problems/MissingInArray.py
@@ -1,6 +1,12 @@
 class Solution:
     def missingNum(self, arr):
-        L = list(range(1, len(arr)+2))
-        for i in L:
-            if i not in arr:
-                return i
+        n = len(arr) + 1
+        expected_sum = n * (n + 1) // 2
+        actual_sum = sum(arr)
+        return expected_sum - actual_sum
+
+
+        # L = list(range(1, len(arr)+2))
+        # for i in L:
+        #     if i not in arr:
+        #         return i
